db07d0ce0a9a83fa76d3792c35faea776b83e5c5
Kenn-CodeCrew <40698252+Kenn-CodeCrew@users.noreply.github.com>
2019-02-07 14:54:12 -0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Print the first p tag to the console using the ID, class, and tag selectors. */
+
+/* Print the special class using both the query selector and query all selector. */
+
+/* Change the color of the h1 statement to blue. */
+
+/* Change the color of the last p tag to yellow. */
