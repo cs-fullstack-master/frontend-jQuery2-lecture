d7bf3e9f17276ff70d701f91b06c5996dd0bb5ce
Kenn <kenn+git@code-crew.org>
2019-02-08 13:41:49 -0600

    update

--- rev1.js
+++ rev1.js
@@ -3,18 +3,24 @@ function intervalTimerSameThingInMyCase()
     // console.log(fourthOfASecondCounter);
     fourthOfASecondCounter++;
 
-    secondsForH2 = parseInt(fourthOfASecondCounter/4);
-    if(secondsForH2 >= 60)
-    {
-        secondsForH2 = 0;
-        fourthOfASecondCounter = 0;
-        minutesForH2++;
-    }
+    // secondsForH2 = parseInt(fourthOfASecondCounter/4);
+    // if(secondsForH2 >= 60)
+    // {
+    //     secondsForH2 = 0;
+    //     fourthOfASecondCounter = 0;
+    //     minutesForH2++;
+    // }
+    //
+    // $("h2").text(minutesForH2+ "m:" + secondsForH2+ "secs");
+    $("h2").text(fourthOfASecondCounter);
 
-    $("h2").text(minutesForH2+ "m:" + secondsForH2+ "secs");
+    if(fourthOfASecondCounter > 20)
+    {
+        clearInterval(intervalID);
+    }
 }
 
 var intervalID = setInterval(intervalTimerSameThingInMyCase, 250);
-var fourthOfASecondCounter = 220;
-var secondsForH2 = 0;
-var minutesForH2 = 0;
+var fourthOfASecondCounter = 0;
+// var secondsForH2 = 0;
+// var minutesForH2 = 0;
