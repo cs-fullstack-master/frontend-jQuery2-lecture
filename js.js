70a08e90132fc2d2a8b4d3465520c59c832996dc
Kenn <kenn+git@code-crew.org>
2019-02-08 09:47:52 -0600

    blah

--- js.js
+++ js.js
@@ -1,7 +1,11 @@
-/* Print the first p tag to the console using the ID, class, and tag selectors. */
+/* Print the first p tag to the console using the ID,
+class, and tag selectors. */
+console.log( $("#first") );
 
 /* Print the special class using both the query selector and query all selector. */
 
+
 /* Change the color of the h1 statement to blue. */
+
 
 /* Change the color of the last p tag to yellow. */
